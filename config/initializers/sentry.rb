3f51c58059dc12556addd137eb99c7fad6c5e07e
KolkovSS <s.kolkov@3commas.io>
2023-07-17 22:52:30 +0300

    Sentry added

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,13 @@
+Sentry.init do |config|
+  config.dsn = 'https://294135d0007d437db4222b7bcb88e9c2@o4505546453811200.ingest.sentry.io/4505546455252992'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+end
