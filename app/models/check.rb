cab01ac04da7d1c8c9c292f5bcfdf9811302fd8f
KolkovSS <s.kolkov@3commas.io>
2023-08-22 21:58:35 +0300

    Add aasm states to checks

--- app/models/check.rb
+++ app/models/check.rb
@@ -2,4 +2,18 @@
 
 class Check < ApplicationRecord
   belongs_to :repository
+
+  aasm whiny_transitions: false, column: :status do
+    state :not_checked, initial: true, display: 'Not checked'
+    state :in_progress, display: 'IP'
+    state :checked, display: 'Checked'
+
+    event :perform_check do
+      transitions from: :not_checked, to: :in_progress
+    end
+
+    event :finish_check do
+      transitions from: :in_progress, to: :checked
+    end
+  end
 end
