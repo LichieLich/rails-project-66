d89d795c692e3f286a0d58bcd0a698c65829d0a7
kolkovss <kolkovss@it.mos.ru>
2023-11-09 23:14:59 +0300

    Moved debug p base url

--- test/controllers/web/repositories/checks_controller_test.rb
+++ test/controllers/web/repositories/checks_controller_test.rb
@@ -4,6 +4,8 @@ require 'test_helper'
 
 class ChecksControllerTest < ActionDispatch::IntegrationTest
   setup do
+    p '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+    p ENV.fetch('BASE_URL', nil)
     @repository = repositories(:one)
     @check = checks(:one)
     @user = users(:one)
@@ -11,7 +13,7 @@ class ChecksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should show test' do
-    p ENV.fetch('BASE_URL', nil)
+    
     get repository_check_url(@repository, @check)
     assert_response :success
   end
