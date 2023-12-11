d8aa3cc589337dc2dd8982c3e9648dd14eebe3bb
kolkovss <kolkovss@it.mos.ru>
2023-12-11 22:21:53 +0300

    Added column to migration

--- db/migrate/20231211185139_add_columns_to_repository_checks.rb
+++ db/migrate/20231211185139_add_columns_to_repository_checks.rb
@@ -1,6 +1,7 @@
 class AddColumnsToRepositoryChecks < ActiveRecord::Migration[7.0]
   def change
     rename_table :checks, :repository_checks
+    add_column :repository_checks, :repository, :integer
     add_column :repository_checks, :passed, :boolean
     rename_column :repository_checks, :status, :aasm_state
   end
