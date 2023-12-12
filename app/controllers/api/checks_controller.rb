df55d26c3f406f9175180225480ad5ed800d4089
kolkovss <kolkovss@it.mos.ru>
2023-12-12 22:34:42 +0300

    Api check creation now can work with json/urlencoded content types

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -6,22 +6,22 @@ module Api
     skip_before_action :verify_authenticity_token, only: :create
 
     def create
-      unless params['commits']
-        logger.info "Recieved a non push event by #{params['hook_id']}"
+      payload = params[:payload] || params
+
+      unless payload['commits']
+        logger.info "Recieved a non push event by #{payload['hook_id']}"
         return
       end
 
-      @repository = Repository.find_by!(github_id: params['repository']['id'])
-      user = User.find_by!(github_id: params['sender']['id'])
+      @repository = Repository.find_by!(github_id: payload['repository']['id'])
+      user = User.find_by!(github_id: payload['sender']['id'])
 
       @check = @repository.checks.build
-      @check.commit_id = params['head_commit']['id']
+      @check.commit_id = payload['head_commit']['id']
       @check.start_check!
       repository_checker.perform_later(user, @check)
       @check.save
-      # binding.irb
 
-      # render status: :ok, json: @controller.to_json
       render json: {}
     end
 
