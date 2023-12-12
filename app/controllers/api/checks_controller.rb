46a0bc506fe1a8202f0d16128797415e96e5b038
kolkovss <kolkovss@it.mos.ru>
2023-12-12 21:44:06 +0300

    render plain in api check controller

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -20,7 +20,8 @@ module Api
       repository_checker.perform_later(user, @check)
       @check.save
 
-      render status: :ok, json: @controller.to_json
+      # render status: :ok, json: @controller.to_json
+      render plain: "OK"
     end
 
     private
