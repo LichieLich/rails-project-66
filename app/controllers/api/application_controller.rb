3ff7fa63b06e57582145c532abbea2a9669edeef
kolkovss <kolkovss@it.mos.ru>
2024-01-02 23:44:52 +0300

    Removed Pundit from API app controller

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -2,10 +2,5 @@
 
 module Api
   class ApplicationController < ActionController::Base
-    include Pundit::Authorization
-
-    def current_user
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
   end
 end
