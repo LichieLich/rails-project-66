ab08a29799ee7bab20185733ee42d0016bfd6a6f
kolkovss <kolkovss@it.mos.ru>
2024-01-20 18:52:28 +0300

    Moved check_helper methods into check_repository_job

--- app/jobs/check_repository_job.rb
+++ app/jobs/check_repository_job.rb
@@ -49,4 +49,19 @@ class CheckRepositoryJob < ApplicationJob
   def github_repository_api
     ApplicationContainer[:github_repository_api]
   end
+
+  def send_complete_notification(user, check)
+    CheckNotificationMailer.with(
+      user:,
+      check:
+    ).check_notification.deliver_later
+  end
+
+  def check_has_no_problems?(check)
+    return false unless check.linter_result
+
+    (check.linter_result[/offense_count":[^0]/] || check.linter_result[/errorCount":[^0]/] ||
+      check.linter_result[/warningCount":[^0]/] || check.linter_result[/fixableErrorCount":[^0]/] ||
+      check.linter_result[/fixableWarningCount":[^0]/]).nil?
+  end
 end
