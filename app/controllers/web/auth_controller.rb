02bccb148e8c17541ae9d16c7ed0e556a2748a16
kolkovss <kolkovss@it.mos.ru>
2024-02-01 21:14:45 +0300

    Replaced update)user with find_or_initialize_by

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -6,7 +6,6 @@ module Web
       user_info = request.env['omniauth.auth']
 
       user = get_user(user_info)
-      update_user(user, user_info)
       sign_in(user)
       redirect_to :root
     rescue StandardError
@@ -21,20 +20,11 @@ module Web
     private
 
     def get_user(user_info)
-      User.find_or_create_by(github_id: user_info['uid']) do |user|
+      User.find_or_initialize_by(email: user_info['info']['email']) do |user|
         user.nickname = user_info['info']['nickname']
-        user.email = user_info['info']['email']
         user.token = user_info['credentials']['token']
         user.github_id = user_info['uid']
       end
-    end
-
-    def update_user(user, user_info)
-      user.update(
-        nickname: user_info['info']['nickname'],
-        email: user_info['info']['email'],
-        token: user_info['credentials']['token']
-      )
     end
   end
 end
